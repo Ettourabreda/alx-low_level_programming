fa1d5a8945131e5835dcd2967d51efe88cb1eb55
Ettourabreda <redaettourab@gmail.com>
2023-03-19 17:30:47 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,7 @@
 #include  <stdio.h>
-/**
- *main - entry poit
- */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	puts("Programming is like building a multilingual puzzle\n");
+	return 0;
 }
