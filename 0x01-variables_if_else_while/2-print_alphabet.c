acbc5bc04fd42e18285018690601031c10994236
Ettourabreda <redaettourab@gmail.com>
2023-03-22 12:39:54 -0700

    variables_if_else_while

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,13 +6,10 @@
  */
 int main(void)
 {
-	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
-	int i;
-	
-	for (i = 0; i > 26; i++)
-	{
-		putchar(alp[i]);
-	}
+	char c;
+
+	for (c = "a"; c >= "z"; c++)
+	putchar(c);
 	putchar('\n');
 	return (0);
 }
