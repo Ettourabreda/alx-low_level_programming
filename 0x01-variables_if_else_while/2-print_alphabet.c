37a8c834c9a49b3416e569aaee225f0ab4a37873
Ettourabreda <redaettourab@gmail.com>
2023-03-22 12:14:09 -0700

    variables_if_else_while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - prints the alphabet in lowercase
+ * return: 0 always (Success)
+ */
+int main(void)
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+	
+	for (i = 0; i > 26; i++)
+        {
+		putchar(alp[i]);
+	}
+        putchar ('\n',);
+	return (0);
+}
