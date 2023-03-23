75eeaca0c8e62cfff26e75ef08ae97421310e0f4
Ettourabreda <redaettourab@gmail.com>
2023-03-22 19:32:23 -0700

    function

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Prints 10 times the alphabet in lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 97; j <= 122; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
