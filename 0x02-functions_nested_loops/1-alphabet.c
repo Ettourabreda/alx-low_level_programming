e759a492a6740c7c07fea90ad83151766ec16c19
Ettourabreda <redaettourab@gmail.com>
2023-03-22 18:45:52 -0700

    functions

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.c"
+/**
+ * main -  prints the alphabet, in lowercase,
+ *
+ * Return: 0 Always (Success)
+ */
+void print_alphabet(void)
+{
+	int c
+	_putchar(c = 'a'; c <= 'z');
+	_putchar("\n");
+	return (0);
+}
