22a6f95dd393eb17b881d325e833d7e2b1581e7c
Ettourabreda <redaettourab@gmail.com>
2023-03-27 17:36:05 -0700

    pointers_arrays_strings

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * int_rev - prints a string, in reverse, followed by a new line.
+ * print_rev - prints a string, in reverse.
  *
  * @s: string
  * Return: Always 0 (Success)
